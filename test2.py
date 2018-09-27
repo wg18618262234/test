0a2980d0108f4e7cff51aebb373abdddbe1af17e
王刚 <wg18618262234@outlook.com>
2018-09-28 00:06:39 +0800

    更新自动挑战下一层boss

--- test2.py
+++ test2.py
@@ -5,9 +5,17 @@ try:
 except ImportError:
     import _thread as thread
 import time
+import json
 
 def on_message(ws, message):
+    global guanqia
+    print('__________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________')
     print(message)
+    if (json.loads(message).get('k') and json.loads(message).get('k')!=int(guanqia)):
+        guanqia = int(guanqia) + 1
+    print('当前挑战关卡是：%s'%guanqia)
+    print('服务器返回关卡是：%s'%json.loads(message).get('k'))
+    print('__________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________')
 
 def on_error(ws, error):
     print(error)
@@ -17,11 +25,13 @@ def on_close(ws):
 
 def on_open(ws):
     def run(*args):
+        global guanqia
         ws.send('{"userName":"18618262234","passWord":"wanggang00","plat":0,"key":"","pktId":0}')
         # ws.send('{"userName":"mao8020586bu","passWord":"luozhenkun","plat":0,"key":"","pktId":0}')
-        ws.send('{"pktId":2}')
         for i in range(1000):
-            time.sleep(1)
+            time.sleep(0.5)
+            ws.send('{"pktId":2}')
+            time.sleep(0.5)
             ws.send('{"levelId":%d,"operate":2,"danci":4,"pktId":5}'%(int(guanqia)))
         time.sleep(1)
         ws.close()
@@ -29,13 +39,13 @@ def on_open(ws):
     thread.start_new_thread(run, ())
 
 
-if __name__ == "__main__":
-    guanqia = input('输入关卡数：')
+while True:
+    # guanqia = input('输入关卡数：')
+    guanqia = 892
     websocket.enableTrace(True)
     ws = websocket.WebSocketApp("ws://47.99.84.144:35001/",
                               on_message = on_message,
                               on_error = on_error,
                               on_close = on_close)
     ws.on_open = on_open
-    if(ws.run_forever()):
-        print(guanqia)
+    ws.run_forever()
